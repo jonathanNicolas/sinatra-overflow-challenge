06e783a1e25012b567055b0627487cd81ef4e001
nil <nilthacker@users.noreply.github.com>
2016-01-07 11:39:36 -0800

    Use t.references for user_id foreign key

--- db/migrate/20160107104209_create_questions.rb
+++ db/migrate/20160107104209_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.integer :user_id
+      t.references :user, :null => false
       t.string :title
       t.string :text
 
