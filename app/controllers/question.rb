c4cc63b8604c51d3e86740b14145d3481929aab7
GregoryW <email": null>
2016-01-08 12:11:47 -0800

    add _question partial

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -61,4 +61,6 @@ get '/questions/:question_id' do
   @question_user = @question.user
   @question_comments = @question.comments
   @question_answers = @question.answers
+  @question_vote_count = @question.votes.count
+  erb :"question/_question"
 end
