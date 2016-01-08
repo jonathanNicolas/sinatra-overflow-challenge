4db602c9a4801abe23115ffdd97d544eeb3ba308
Nil Thacker <nil@nil.gs>
2016-01-08 15:00:40 -0800

    Implement Question#time_since_creation to return a string that represents how long ago the question was posted.

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,7 +11,16 @@ class Question < ActiveRecord::Base
   validates :vote_total, presence: true
 
   def time_since_creation
-    ((Time.now - created_at) / 3600).round
-  end
+    hours_since_creation = ((Time.now - created_at) / 3600).round
+    return "less than an hour ago" if hours_since_creation == 0
+    time_since = (hours_since_creation / 24).to_s
+    if time_since.to_i > 0
+      time_since += " day" if time_since.to_i == 1
+      time_since += " days" if time_since.to_i > 1
+    else
+      time_since = hours_since_creation.to_s + " hours"
+    end
 
+    return time_since
+  end
 end
