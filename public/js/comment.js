e82a1dc9b12588a307913f07c2507836280a7f40
Nil Thacker <nil@nil.gs>
2016-01-08 18:32:04 -0800

    Middle of getting answer comment ajax working

--- public/js/comment.js
+++ public/js/comment.js
@@ -33,18 +33,19 @@ $(document).ready(function () {
     });
 
   // answer add comment button clicked
-  $('.container').on('click', '#question-add-comment', function(event){
+  $('.container').on('click', '.answer-add-comment', function(event){
       event.preventDefault();
-      $('.new-comment-container-question').show();
+      $(this).parent().parent().children(".new-comment-container").show();
     });
 
   // answer form submit button clicked
-  $('.container').on('submit', '#question_comment', function(event){
+  $('.container').on('submit', '#answer_comment', function(event){
       "use strict"
-      var allData
+      var allData, formInfo
       event.preventDefault();
 
       allData = $(this).serialize();
+      formInfo = $(this)
 
       var ajaxRequest = $.ajax({
         url: "/comments/new",
@@ -53,9 +54,11 @@ $(document).ready(function () {
       })
 
       ajaxRequest.done(function(successResponse) {
-        $('.all-comments').append(successResponse);
-        $('.new-comment-container-question').hide();
-        $("#question_comment")[0].reset();
+        formInfo.parent().parent().append(successResponse);
+        // formInfo.parent().parent('.all-comments').css("background-color", "yellow");
+        // $('.new-comment-container-question').hide();
+        formInfo.hide();
+        // $("#question_comment")[0].reset();
       });
 
       ajaxRequest.fail(function(failResponse) {
