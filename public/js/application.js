3dbc84591dc22002fc73a26d771b323f6636b2ff
Mia Lehrer <mia.lehrer@gmail.com>
2016-01-08 18:08:37 -0800

    Add Ajax arrow functionality

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,42 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
+  upvote();
+  downvote();
 });
+
+var upvote = function(){
+  $("div.vote-container").on('click', '.upvote', function(e){
+    e.preventDefault()
+    addUpVote(this)
+  })
+};
+
+var downvote = function(){
+  $("div.vote-container").on('click', '.downvote', function(e){
+    e.preventDefault()
+    addDownVote(this)
+  });
+};
+
+var addDownVote = function(a){
+  $.ajax({
+    method: 'GET',
+    url: a['href']
+  })
+  .done(function(voteStuff){
+    var voteObject = JSON.parse(voteStuff)
+    $(".vote-count#" + voteObject["type"]+ voteObject["id"]).html(voteObject["votes"])
+    $(".fa-caret-down#"+ voteObject["type"]+ voteObject["id"]).css("color", "orange")
+  })
+};
+
+var addUpVote = function(a){
+  $.ajax({
+    method: 'GET',
+    url: a['href']
+  })
+  .done(function(voteStuff){
+    var voteObject = JSON.parse(voteStuff)
+    $(".vote-count#" + voteObject["type"]+ voteObject["id"]).html(voteObject["votes"])
+    $(".fa-caret-up#"+ voteObject["type"]+ voteObject["id"]).css("color", "orange")
+  })
+};
