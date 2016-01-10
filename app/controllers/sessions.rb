8afe1d06766cc4e9bb061de0f8a2e78b523e5991
Nil Thacker <nil@nil.gs>
2016-01-09 22:43:35 -0800

    Remove legacy authentication code from sessions.rb

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -40,30 +40,3 @@ get '/callback' do
     redirect session[:referrer]
   end
 end
-
-# get '/login' do
-#   if session[:user_id]
-#     @user = User.find(session[:user_id])
-#     redirect "/users/#{@user.id}"
-#   else
-#     erb :login
-#   end
-# end
-
-# post '/login' do
-#   @user = User.find_by_email(params[:email])
-#   if @user && @user.authenticate(params[:password_plaintext])
-#     session[:user_id] = @user.id
-#     redirect "/users/#{@user.id}"
-#   else
-#     session.delete(:user_id)
-#     @error = "Please check your email address and password and try again."
-#     erb :login
-#   end
-# end
-
-# get '/logout' do
-#   session.delete(:user_id)
-#   redirect '/'
-# end
-
